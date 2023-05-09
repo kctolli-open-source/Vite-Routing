b3eb014db38c67a8996efaa9ef35b0c9a43b385e
kctolli <developer@ktolliver.org>
2023-05-08 22:38:05 -0600

    basic Router

---
+++ basic-route/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p>This is a Basic React app to demonstrate vite routing.</p>
+      <p>Current Path: {window.location.pathname}</p>
+    </>
+  );
+}
+
+export default App;
